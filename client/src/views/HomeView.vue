324782f4d286ba311ea549b755f9dcfa3bc69e87
wadpas <wadpas@gmail.com>
2024-11-25 14:31:40 +0200

    done query filters

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,4 +2,61 @@
   <div>
     <h1>This is an home page</h1>
   </div>
+
+  <button v-on:click="setParams(2020)">2020</button>
+  <button v-on:click="setParams(2024)">2024</button>
+
+  {{ books }}
 </template>
+
+<script setup>
+import axios from "axios"
+import { onMounted, ref, watch, onUpdated } from "vue"
+import { useRoute, useRouter } from "vue-router"
+
+const router = useRouter()
+const route = useRoute()
+
+let books = ref([])
+
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3000/api/v1/",
+})
+
+let params = {
+  year: 2014,
+}
+
+function setParams(year) {
+  params.year = year
+  console.log(params.year)
+  router.push({ path: "/", query: { year: params.year } })
+  console.log(params)
+}
+
+// router.afterEach((to, from) => {
+//   axiosInstance.get("/books", { params }).then((res) => {
+//     books.value = res.data
+//     console.log(books.value)
+//   })
+// })
+watch(
+  () => route.query,
+  (query) => {
+    params.year = query.year
+    console.log(params.year)
+    axiosInstance.get("/books", { params }).then((res) => {
+      books.value = res.data
+      console.log(books.value)
+    })
+  }
+)
+
+onMounted(() => {
+  console.log("home page mounted")
+  axiosInstance.get("/books").then((res) => {
+    books.value = res.data
+    console.log(books.value)
+  })
+})
+</script>
--- server/controllers/books_v1.js
+++ server/controllers/books_v1.js
@@ -1,7 +1,7 @@
 const Book = require("../models/book")
 
 const getBooks = async (req, res) => {
-  const { title, author, genre, year, sort } = req.query
+  const { title, author, genre, year, sort, fields, numericFilters } = req.query
   const queryObject = {}
   if (title) {
     queryObject.title = { $regex: title, $options: "i" }
@@ -15,7 +15,26 @@ const getBooks = async (req, res) => {
   if (year) {
     queryObject.year = year
   }
-  console.log(queryObject)
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    }
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g
+    let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+
+    const options = ["year"]
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-")
+      console.log(field, operator, value)
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) }
+      }
+    })
+  }
   let result = Book.find(queryObject)
   if (sort) {
     const sortList = sort.split(",").join(" ")
@@ -23,6 +42,16 @@ const getBooks = async (req, res) => {
   } else {
     result = result.sort("createdAt")
   }
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ")
+    result = result.select(fieldsList)
+  }
+
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+  result = result.skip(skip).limit(limit)
+
   const books = await result
   res.status(200).json({ books, nbHits: books.length })
 }
